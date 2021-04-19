0a1eb7c849ad115ee5cbcddbee87e91f7b54a8d2
KlaysKAS <breakitedkirill@mail.ru>
2021-04-19 21:42:33 +1000

    Created Pattern to start project

---
+++ jsripts.js
@@ -0,0 +1,10 @@
+function draw(){
+	var canvas = document.getElementById('canvasBg');
+	if (canvas.getContext){
+		var ctx = get.Context("2d");
+	} else {
+	Document.write("Ваш браузер не поддерживает canvas");		
+	}
+	
+
+}
