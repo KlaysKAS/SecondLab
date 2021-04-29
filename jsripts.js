3b854864e850dbbc3a318365cfcd61dc4ada41fa
KlaysKAS <breakitedkirill@mail.ru>
2021-04-29 20:10:46 +1000

    Start work
    added addaptivity for canvas and background

--- jsripts.js
+++ jsripts.js
@@ -1,10 +1,51 @@
-function draw(){
-	var canvas = document.getElementById('canvasBg');
-	if (canvas.getContext){
-		var ctx = get.Context("2d");
-	} else {
-	Document.write("Ваш браузер не поддерживает canvas");		
-	}
-	
-
+function init () {
+	canvas = document.getElementById("myCanvas");
+	ctx = canvas.getContext("2d");
+	var bodySize = document.body.getBoundingClientRect();
+	canvas.setAttribute("width", bodySize.width);
+	canvas.setAttribute("height", bodySize.height);
 }
+
+
+
+var canvas;
+var ctx;
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function keyDownHandler(e) {
+	if (e.keyCode == 39) {	
+		rightPressed = true;
+	} else if (e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if (e.keyCode == 39) {
+		rightPressed = false;
+	} else if (e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+draw();
+
+//requestAnimationFrame(draw);
